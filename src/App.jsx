19ed2bbfb4e7b76eb3f6fb742182be920c24f4bb
Viral Jain <veerljain1234@gmail.com>
2024-04-14 16:05:12 +0530

    Added: Assests and folder structure

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="font-extrabold text-center bg-gray-200 ">
+      hello world
+    </div>
+  )
+}
+
+export default App
