6a701ed020edd15035080889da01c7f4ae4d38b5
Viral Jain <veerljain1234@gmail.com>
2024-04-16 16:39:40 +0530

    feat: Added video carousel to Highlights section with GSAP animation

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -2,6 +2,7 @@ import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
 import React from 'react'
 import { rightImg, watchImg } from '../utils'
+import VideoCarousel from './VideoCarousel'
 
 const Highlights = () => {
 
@@ -27,8 +28,10 @@ const Highlights = () => {
                         </p>
                     </div>
                 </div>
-            </div>
 
+                <VideoCarousel />
+
+            </div>
         </section>
 
     )
